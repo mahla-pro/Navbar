1a8d3f0db86834524e2acb09d9f81e420929360a
mahla-pro <hi@mahla.pro>
2023-08-29 15:10:06 +0430

    first

---
+++ src/Navbar.js
@@ -0,0 +1,74 @@
+import React ,{useRef, useState,useEffect}from 'react';
+import logo from './logo.png';
+import {FaBars,FaTwitter,FaTimes} from 'react-icons/fa';
+import {links,socials} from './data'
+
+const Navbar = () => {
+    
+const[showLink,setShowList]=useState(false);
+const linksContainerRef=useRef(null);
+const linksRef= useRef(null);
+
+useEffect(() => {
+  const linkHeight=linksRef.current.getBoundingClientRect().height;
+  if(showLink){
+      linksContainerRef.current.style.height=`${linkHeight}px`
+  }
+  else{
+    linksContainerRef.current.style.height="0px"
+  }
+
+}, [showLink]);
+
+
+    return (
+        <nav>
+            <div className='container'>
+                <div className='nav-logo'>
+                    <button className='nav-toggle' onClick={()=>setShowList(!showLink)}>
+                        {
+                              showLink ? <FaTimes/> :<FaBars/>
+                        }
+                       
+                    </button>
+                    <img src={logo} alt=""/>
+                </div>
+               
+                        
+                    <div className='nav-links' ref={linksContainerRef}>
+                    <ul className='list'  ref={linksRef}>
+                        {
+                            links.map((link)=>{
+                                const {id,url,text}=link;
+                                return(
+                                    <li key={id}>
+                                        <a href={url}>{text}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+
+                    
+                </div>
+             
+       
+                <ul className='nav-social'>
+                {
+                    socials.map((social)=>{
+                        const {id,url,icon}=social;
+                        return(
+                            <li key={id}>
+                                <a href={url}>{icon}</a>
+                            </li>
+                        )
+                    })
+                }
+                </ul>
+
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {FaBehance,FaFacebook,FaLinkedin,FaTwitter} from 'react-icons/fa';
+
+
+export const links =[
+    {
+        id:1,
+        url:'/',
+        text:'خانه',
+    },
+    {
+        id:2,
+        url:'/',
+        text:'درباره ما',
+    },
+    {
+        id:3,
+        url:'/',
+        text:'تماس با ما',
+    },
+    {
+        id:4,
+        url:'/',
+        text:'پروژه ها' ,
+    }
+
+]
+
+
+export const socials =[
+ {
+     id:1,
+     url:'https://twitter.com',
+     icon: <FaTwitter/>
+ },
+ {
+    id:2,
+    url:'https://Behance.com',
+    icon: <FaBehance/>
+},
+{
+    id:3,
+    url:'https://facebook.com',
+    icon: <FaFacebook/>
+},
+{
+    id:4,
+    url:'https://linkedin.com',
+    icon: <FaLinkedin/>
+},
+
+]
